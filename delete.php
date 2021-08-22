c223e4b318002beefe5692e3f45b687c123ac0ef
Tanvir-web-developer <tanvirtanuu@hotmail.com>
2021-08-22 13:47:59 +0600

    Delete-ii updated

--- delete.php
+++ delete.php
@@ -1,9 +1,27 @@
-<?php include 'header.php'; ?>
+<?php 	include 'header.php'; 
+		include 'config.php';
+
+if(isset($_POST['deletebtn'])){
+
+
+ $stu_id = $_POST ['sid'];
+ 
+ $sql = "DELETE FROM student WHERE sid = {$stu_id}";
+ $result = mysqli_query($conn, $sql) or die ("Query Unsuccessfull.");
+
+ header("location: http://localhost/crud");
+
+ mysqli_close($conn);
+
+}
+
+
+?>
 
 
 <div id="main-content">
     <h2>Delete Record</h2>
-    <form class="post-form" action="" method="post">
+    <form class="post-form" action="<?php echo $_SERVER ['PHP_SELF']; ?>" method="post">
         <div class="form-group">
             <label>Id</label>
             <input type="text" name="sid" />
